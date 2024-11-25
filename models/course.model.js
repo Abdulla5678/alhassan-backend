f49607b1cf9ffedcd62701f2e1d19b5ea9aeaee4
abdullah <abdallaashrf155@gmail.com>
2024-11-25 09:14:35 +0200

    form update

--- models/course.model.js
+++ models/course.model.js
@@ -1,51 +1,55 @@
-
 const mongoose = require('mongoose');
 
-const courseScheme = new mongoose.Schema({
-    name: {
-        type :String,
-        required: true
+const courseSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        age: {
+            type: String,
+            required: true,
+        },
+        nationality: {
+            type: String,
+            required: true,
+        },
+        whatsappNumber: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        studyBefore: {
+            type: String,
+            required: true,
+        },
+        readingLevel: {
+            type: String,
+            required: true,
+        },
+        writingLevel: {
+            type: String,
+            required: true,
+        },
+        speakingLevel: {
+            type: String,
+            required: true,
+        },
+        listeningLevel: {
+            type: String,
+            required: true,
+        },
+        goal: {
+            type: String,
+            required: true,
+        },
     },
-    age: {
-        type :String,
-        required: true
-    },
-    nationality: {
-        type :String,
-        required: true
-    },
-    whatsappNumber: {
-        type :String,
-        required: true
-    },
-    email: {
-        type :String,
-        required: true
-    },
-    studyBefore: {
-        type :String,
-        required: true
-    },
-    readingLevel: {
-        type :String,
-        required: true
-    },
-    writingLevel: {
-        type :String,
-        required: true
-    },
-    speakingLevel: {
-        type :String,
-        required: true
-    },
-    listeningLevel: {
-        type :String,
-        required: true
-    },
-    goal: {
-        type :String,
-        required: true
-    },
-})
+    {
+        timestamps: true, // Automatically adds createdAt and updatedAt fields
+    }
+);
 
-module.exports = mongoose.model('Course',courseScheme)
+module.exports = mongoose.model('Course', courseSchema);
